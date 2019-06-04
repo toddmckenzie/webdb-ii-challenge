fcd2e826aa777235d9cc5dd515d64dd0a9f89b10
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-06-04 14:54:57 -0500

    building out CRUD operations

--- zoos/zoosRouter.js
+++ zoos/zoosRouter.js
@@ -18,11 +18,60 @@ router.get('/', (req, res) => {
         res.json(result)
     })
     .catch(error => {
-        res.status(500).json({ message: 'internal server error'})
+        res.status(500).json({ message: 'internal server error.'})
+    })
+})
+
+router.get('/:id', (req, res) => {
+    db(req.params.id)
+    .then(result => {
+        res.json(result)
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'Internal server error.'})
     })
 })
 
 
+router.post('/', (req, res) => {
+    const post = req.body;
+    db(req.body)
+    .then(result => {
+        res.status(201).json(result)
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'Internal server error.'})
+    })
+})
+
+router.put('/:id', (req, res) => {
+    const post = { ...req.body };
+    const id = req.params.id;
+
+    db(id, post)
+    .then(result => {
+        res.json(result)
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'Internal server error.'})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    db(id)
+    .then(count => {
+        if (count > 0){
+            return res.status(200).json(count)
+        } else {
+            return res.status(404).json({ message: 'User not found'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'Internal server error'})
+    })
+})
 
 
 module.exports = router;
